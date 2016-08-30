733b9e5904f63ac14782e5dfc3db3fd842fe8c71
NgocNguyen <ngoc.nguyenvan@powergatevietnam.co>
2016-08-30 16:14:23 +0700

    no message

--- deploy/auto-deploy.js
+++
@@ -1,6 +0,0 @@
-'use trict'
-var sys = require('sys')
-var exec = require('child_process').exec;
-function puts(error, stdout, stderr) { sys.puts(stdout) }
-var options = {};
-exec("git pull && git status && git submodule sync && git submodule update && git submodule status", options, puts);
--- deploy/auto-deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-exec node ./deploy/pull-code.js
-
