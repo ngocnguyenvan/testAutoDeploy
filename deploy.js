b24a28f87ae0c8e7d44324cee0de0b28b4735185
NgocNguyen <ngoc.nguyenvan@powergatevietnam.co>
2016-08-31 14:51:28 +0700

    no message

--- deploy.js
+++ deploy.js
@@ -2,8 +2,9 @@ var http = require('http');
 var sys = require('sys')
 var exec = require('child_process').exec;
 http.createServer(function(req, res){
-	function puts(error, stdout, stderr) { sys.puts(stdout) }
-	var options = {};
-	exec("git pull && git status && git submodule sync && git submodule update && git submodule status", options, puts);
+	/*function puts(error, stdout, stderr) { sys.puts(stdout) }
+	var options = {};*/
+	exec("git pull && git status && git submodule sync && git submodule update && git submodule status");
+	exec("npm install");
 }).listen(7812, "192.168.1.8");
 console.log('Server running at http://192.168.1.8:7812/');
