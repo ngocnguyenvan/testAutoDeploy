dab145b3aae3c130972241209f6bc0b5b8c56d53
NgocNguyen <ngoc.nguyenvan@powergatevietnam.co>
2016-08-31 14:38:57 +0700

    no message

--- deploys/pull-code.js
+++ deploys/pull-code.js
@@ -3,4 +3,4 @@ var sys = require('sys')
 var exec = require('child_process').exec;
 function puts(error, stdout, stderr) { sys.puts(stdout) }
 var options = {};
-exec("git pull && git status && git submodule sync && git submodule update && git submodule status", options, puts);
+exec("git pull && git status && git submodule sync && git submodule update && git submodule status && npm install", options, puts);
